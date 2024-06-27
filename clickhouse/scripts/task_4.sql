15da5fd9c291349fdc00f5b700a2835f16619621
AinKube <aleksandr.2000.ru@yandex.ru>
2024-06-27 06:43:26 +0300

    mod: fix stg_writer rights

--- clickhouse/scripts/task_4.sql
+++ clickhouse/scripts/task_4.sql
@@ -14,3 +14,8 @@ create role stg_writer;
 grant create table, insert on stg.* to stg_writer;
 
 CREATE USER stg_writer IDENTIFIED WITH SHA256_password BY 'stg_qwerty' DEFAULT ROLE stg_writer;
+
+
+-- Дадим пользователю stg_writer возможность заливать данные в созданную буферную таблицу
+
+grant insert on direct_log.rating_wh_by_suppliers to stg_writer;
